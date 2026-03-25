9aece177a95bb6494e0cdf32071c75e773096960
kajratsungat6-afk <kajratsungat6@gmail.com>
2026-03-25 10:25:12 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication13.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	int arr[100][100];
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> arr[i][j];
+		}
+	}
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << arr[j][i] << ' ';
+		}
+		cout << endl;
+	}
+	return 0;
+}
